db2d966e8e7bd7bcdd254b8d29d1b63082982e8a
Blackchick15 <109077737+Blackchick15@users.noreply.github.com>
2023-09-27 19:22:05 +0300

    commit

---
+++ Js/swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiperHeadphones ", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button__next",
+      prevEl: ".swiper-button__prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+  
